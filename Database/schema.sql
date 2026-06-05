9c01f93d9bbc97a0a68d871ba2693020fe54afb4
miiinhhh <minhming5542@gmail.com>
2026-06-05 16:46:09 +0700

    upd database

--- Database/schema.sql
+++ Database/schema.sql
@@ -79,7 +79,23 @@ CREATE TABLE Playlists
         FOREIGN KEY(OwnerId)
         REFERENCES AspNetUsers(Id)
 );
+CREATE TABLE PlayListTrack
+(
+    PlaylistId UNIQUEIDENTIFIER NOT NULL,
+    MediaId UNIQUEIDENTIFIER NOT NULL,
+    SortOrder INT DEFAULT 0,    -- Cột thêm vào: Giúp kéo thả, sắp xếp thứ tự bài hát
+    
+    PRIMARY KEY (PlaylistId, MediaId), 
 
+    CONSTRAINT FK_PlaylistItems_Playlists
+        FOREIGN KEY (PlaylistId) REFERENCES Playlists(PlaylistId) 
+        ON DELETE CASCADE, -- Nếu xóa playlist thì tự động xóa các liên kết bên trong
+
+    -- Khóa ngoại liên kết tới bảng MediaItems (Track)
+    CONSTRAINT FK_PlaylistItems_MediaItems
+        FOREIGN KEY (MediaId) REFERENCES MediaItems(MediaId)
+        ON DELETE CASCADE -- Nếu bài hát bị xóa khỏi hệ thống thì tự động biến mất khỏi playlist
+);
 CREATE TABLE MediaShares
 (
     ShareId UNIQUEIDENTIFIER PRIMARY KEY,
